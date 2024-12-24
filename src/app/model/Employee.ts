70920ac207ab9af3fe96beb94325080b909ed3b7
Nisar Ahmed Mohammed <nisarahmedmohammed@Mac.lan>
2024-12-24 13:51:48 -0600

    Added A form

---
+++ src/app/model/Employee.ts
@@ -0,0 +1,24 @@
+export class EmployeeModel{
+    empId:number;
+    name:string;
+    emaild:string;
+    city:string;
+    state:string;
+    contactNo:number;
+    address:string;
+    pincode:string;
+
+    constructor(){
+
+        this.empId=0;
+        this.name="";
+        this.emaild="";
+        this.city="";
+        this.state="";
+        this.contactNo=0;
+        this.address="";
+        this.pincode="";
+
+
+    }
+}
