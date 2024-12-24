c3c096fdd80ab1e07e324aa550f141a6aac82e36
Nisar Ahmed Mohammed <nisarahmedmohammed@Mac.lan>
2024-12-24 12:40:05 -0600

    Initial commit

---
+++ src/app/Pages/employee-registration/employee-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-registration',
+  imports: [],
+  templateUrl: './employee-registration.component.html',
+  styleUrl: './employee-registration.component.css'
+})
+export class EmployeeRegistrationComponent {
+
+}
